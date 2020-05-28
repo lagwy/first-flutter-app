ac04a60a6f2f7332accea3d7a4aecb8c353eab4b
Luis Martínez <lagwyf@gmail.com>
2020-05-27 22:54:03 -0500

    upload full directory

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords()
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions()
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) { // itemBuilder callback is called once per suggested word pairing, and places each suggestion into a ListTile row.
+        if (i.isOdd) return Divider(); // Add a one-pixel-high divider before each row in the ListView
+
+        final index = i ~/ 2; // This expression divides i by 2 and returns an integer result.
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); // End of the list reached, generate 10 more word pairings.
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont
+      ),
+    );
+  }
+
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
